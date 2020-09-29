0238e1f0323df23ad03e6a0a31c9b003c5999ec9
magdii <magdysala@gmail.com>
2020-09-29 16:03:01 +0300

    custody  module

--- ncss_custody_request/models/custody_request.py
+++ ncss_custody_request/models/custody_request.py
@@ -8,6 +8,8 @@ class CustodyRequest(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = "employee_id"
 
+    expense_account_move_id = fields.Many2one('account.move', 'Journal Expense')
+    liquidated_account_move_id = fields.Many2one('account.move', 'Journal Liquidated')
     employee_id = fields.Many2one('hr.employee', 'Employee')
     description = fields.Text()
     reason = fields.Text()
@@ -15,7 +17,10 @@ class CustodyRequest(models.Model):
     remaining_amount = fields.Float(compute='get_remaining_amount')
     date = fields.Date()
     exchange_item_ids = fields.One2many('custody.request.line', 'custody_id')
-    move_line_ids = fields.One2many('account.move.line', 'custody_id')
+    move_line_ids = fields.One2many('account.move.line', 'custody_id',
+                                    domain=lambda self: [('move_id', '=', self.expense_account_move_id.id)])
+    liquidated_move_line_ids = fields.One2many('account.move.line', 'custody_id',
+                                               domain=lambda self: [('move_id', '=', self.liquidated_account_move_id.id)])
     is_direct_manager = fields.Boolean(compute='get_direct_manager')
     is_liquidated = fields.Boolean(compute='get_is_liquidated')
     state = fields.Selection([('draft', 'Draft'),
@@ -104,6 +109,7 @@ class CustodyRequest(models.Model):
             'partner_id': address_home_id,
             'custody_id': self.id,
         })
+        return account_move_id
 
     def paid_action(self):
         journal = self.env.user.company_id.custody_journal_id.id
@@ -112,7 +118,8 @@ class CustodyRequest(models.Model):
         credit_account_id = self.env.user.company_id.credit_account_id.id
         amount = self.amount
         address_home_id = self.employee_id.address_home_id.id
-        self.create_account_move(journal, label, debit_account_id, credit_account_id, amount, address_home_id)
+        account_move_obj = self.create_account_move(journal, label, debit_account_id, credit_account_id, amount, address_home_id)
+        self.expense_account_move_id = account_move_obj.id
         self.state = 'paid'
 
     def in_progress_action(self):
@@ -125,10 +132,11 @@ class CustodyRequest(models.Model):
         journal = self.env.user.company_id.custody_journal_id.id
         label = self.env.user.company_id.label
         credit_account_id = self.env.user.company_id.debit_account_id.id
-        debit_account_id = self.env.user.company_id.credit_account_id.id
+        debit_account_id = self.env.user.company_id.expense_account_id.id
         amount = self.amount
         address_home_id = self.employee_id.address_home_id.id
-        self.create_account_move(journal, label, debit_account_id, credit_account_id, amount, address_home_id)
+        account_move_obj = self.create_account_move(journal, label, debit_account_id, credit_account_id, amount, address_home_id)
+        self.liquidated_account_move_id = account_move_obj.id
         self.state = 'done'
 
     def set_to_draft(self):
--- ncss_custody_request/models/setting.py
+++ ncss_custody_request/models/setting.py
@@ -7,6 +7,8 @@ class ResCompany(models.Model):
     debit_account_id = fields.Many2one('account.account')
     credit_account_id = fields.Many2one('account.account')
     custody_journal_id = fields.Many2one('account.journal')
+    expense_account_id = fields.Many2one('account.account')
+    expense_label = fields.Char()
     label = fields.Char()
 
 
@@ -22,18 +24,26 @@ class RequisitionConfigSettings(models.TransientModel):
     custody_journal_id = fields.Many2one('account.journal',
                                         default=lambda self: self.env.user.company_id.custody_journal_id)
 
+    expense_account_id = fields.Many2one('account.account',
+                                         default=lambda self: self.env.user.company_id.expense_account_id)
+    expense_label = fields.Char(default=lambda self: self.env.user.company_id.expense_label)
+
     label = fields.Char(default=lambda self: self.env.user.company_id.label)
 
     @api.model
     def create(self, vals):
         if 'company_id' in vals\
                 or 'debit_account_id' in vals \
+                or 'expense_account_id' in vals \
+                or 'expense_label' in vals \
                 or 'label' in vals \
                 or 'credit_account_id' in vals or 'custody_journal_id' in vals:
             self.env.user.company_id.write({
                     'debit_account_id': vals['debit_account_id'],
                     'credit_account_id': vals['credit_account_id'],
                     'custody_journal_id': vals['custody_journal_id'],
+                    'expense_account_id': vals['expense_account_id'],
+                    'expense_label': vals['expense_label'],
                     'label': vals['label'],
                  })
         res = super(RequisitionConfigSettings, self).create(vals)
