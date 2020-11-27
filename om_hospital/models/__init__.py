84af59f0218389d8943520292d899501825c21a5
elsayedh <Oudounce.11@gmail.com>
2020-11-27 16:49:15 +0300

    Update1

--- om_hospital/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import appointment
-from . import doctor
-from . import lab
-from . import patient
-from . import settings
-
--- om_hospital/reports/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import appointment
-from . import patient_card
-from . import patient_card_xls
