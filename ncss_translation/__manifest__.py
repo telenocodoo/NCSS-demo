b600e5792cd98b0512692db1368b789a4fc5e5cd
magdii <magdysala@gmail.com>
2021-04-08 15:45:18 +0300

    notes translation

---
+++ ncss_translation/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ncss translation',
+    'version': '13.0.1',
+    'summary': 'notes translation',
+    'category': 'hr',
+    'author': 'Magdy,TeleNoc',
+    'description': """
+    notes translation
+    """,
+    'depends': ['base', 'mail', 'note',],
+    'data': []
+}
