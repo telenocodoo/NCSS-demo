92514cbb1d4fe719c5712e898914d7b620ceecf4
magdii <magdysala@gmail.com>
2021-04-11 10:24:12 +0300

    all translation

---
+++ ncss_all_translation/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ncss all translation',
+    'version': '13.0.1',
+    'summary': 'ncss all translation',
+    'category': 'hr',
+    'author': 'Magdy,TeleNoc',
+    'description': """
+    all translation
+    """,
+    'depends': ['base', 'mail'],
+    'data': []
+}
